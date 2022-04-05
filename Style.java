eeb861260cb4e2adad9acc2963a73c834068092c
Matteo Mirasolo <kiurem66@gmail.com>
2022-04-05 11:57:54 +0200

    adde all styles

--- Style.java
+++ Style.java
@@ -25,6 +25,12 @@ public abstract class Style implements Skill{
             default: return 0;
         }
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Style)) return false;
+        return ((Style)obj).getName().equals(getName());
+    }
     
     public static class Coltelli extends Style{
         public Coltelli(){
@@ -56,6 +62,83 @@ public abstract class Style implements Skill{
         @Override
         public String getName() {
             return "Armi da lancio";
+        }
+    }
+
+    public static class Desperado extends Style{
+        public Desperado(){
+            level = 1;
+        }
+        
+        @Override
+        public String getName() {
+            return "Desperado";
+        }
+    }
+
+    public static class Cinematografico extends Style{
+        public Cinematografico(){
+            level = 1;
+        }
+        
+        @Override
+        public String getName() {
+            return "Stile cinematografico";
+        }
+    }
+
+    public static class Marziali extends Style{
+        public Marziali(){
+            level = 1;
+        }
+        
+        @Override
+        public String getName() {
+            return "Arti Marziali";
+        }
+    }
+
+    public static class Acrobatico extends Style{
+        public Acrobatico(){
+            level = 1;
+        }
+        
+        @Override
+        public String getName() {
+            return "Combattimento Acrobatico";
+        }
+    }
+
+    public static class Sporco extends Style{
+        public Sporco(){
+            level = 1;
+        }
+        
+        @Override
+        public String getName() {
+            return "Gioco Sporco";
+        }
+    }
+
+    public static class Libre extends Style{
+        public Libre(){
+            level = 1;
+        }
+        
+        @Override
+        public String getName() {
+            return "Lucha Libre";
+        }
+    }
+
+    public static class Strada extends Style{
+        public Strada(){
+            level = 1;
+        }
+        
+        @Override
+        public String getName() {
+            return "Rissa da Strada";
         }
     }
 
