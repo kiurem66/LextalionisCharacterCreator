84cad02a87001470552e892be630af1e616dd4c0
Matteo Mirasolo <kiurem66@gmail.com>
2022-04-07 00:25:15 +0200

    added gui first elements

--- Gui.java
+++ Gui.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Gui {
     private Character character;
     private JFrame window;
+    JTextField nome;
+    JTextField sentiero;
+    JTextField fazione;
+    JComboBox<String> clan;
     public Gui(){
         window = new JFrame("LexTalionis Character Creator");
         window.setVisible(true);
@@ -10,6 +15,41 @@ public class Gui {
         ImageIcon img = new ImageIcon("sabbat.jpg");
         window.setIconImage(img.getImage());
         window.setSize(800, 600);
+        JPanel panel = new JPanel(new BorderLayout());
+        window.add(panel);
+        JPanel internPanel = new JPanel();
+        panel.add(internPanel, BorderLayout.CENTER);
+
+        internPanel.add(new JLabel("Nome "));
+        nome = new JTextField();
+        nome.setColumns(10);
+        internPanel.add(nome);
+
+        internPanel.add(new JLabel("Sentiero "));
+        sentiero = new JTextField();
+        sentiero.setColumns(10);
+        internPanel.add(sentiero);
+
+
+        internPanel.add(new JLabel("Fazione "));
+        fazione = new JTextField();
+        fazione.setColumns(10);
+        internPanel.add(fazione);
+
+        buildList();
+        internPanel.add(new JLabel("Clan "));
+        internPanel.add(clan);
+
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    void buildList(){
+        String clanlist[] = new String[23];
+        for(int i=0; i<23; i++){
+            clanlist[i] = ClanSelector.get(i);
+        }
+        clan = new JComboBox<String>(clanlist);
     }
 
     public static void main(String[] args) {
