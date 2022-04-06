46b5ebc366ee086d70535175e0ef2e1625e59fd8
Matteo Mirasolo <kiurem66@gmail.com>
2022-04-06 23:42:50 +0200

    Bugfix

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Character c = new Clan.Ventrue();
-        c.searchDisc("Dominazione").incrLevel();
-        c.searchInfl("Esercito").setLevel(3);
-        System.out.println(c.getRemainingPx()); 
+
+        Gui g = new Gui();
     }
 }
